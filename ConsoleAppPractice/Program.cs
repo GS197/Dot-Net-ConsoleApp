3b0e63718aaa589e148cce60451db5682680b293
GIDDALURU SISINDRI <gs197c@gmail.com>
2026-07-27 06:23:26 +0530

    First Commit

---
+++ ConsoleAppPractice/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        StartApplication();
+
+        Console.ReadLine();
+    }
+
+    static void StartApplication()
+    {
+        Login();
+
+        Console.WriteLine("Application Started");
+    }
+
+    static void Login()
+    {
+        ValidateUser();
+
+        Console.WriteLine("User Logged In");
+    }
+
+    static void ValidateUser()
+    {
+        Console.WriteLine("Validating User");
+    }
+}
